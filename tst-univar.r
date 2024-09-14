e611e0e67b1baf9c285ab264581013cb01e3764c
MA Jian <majian1979@qq.com>
2024-09-14 17:18:06 +0800

    Add files via upload

--- tst-univar.r
+++ tst-univar.r
@@ -1,14 +1,16 @@
 library(copent)
 library(twosamples)
 library(robusTest)
+library(R2sample)
+library(tnl.Test)
 library(latex2exp)
 
 sample0 = matrix( rnorm(300,0,1), 300, 1)
 stat1 = cvm1 = ks1 = kuiper1 = wass1 = dts1 = ad1 = wilcox1 = kruskal1 = wilcox2 = vartest1 = 0
+lr1 = za1 = zk1 = zc1 = tnl1 = 0
 for(i in 1:10){
-  # sample1 = matrix( rnorm(350,(i-1),1), 350, 1) # simulation1 : mean
-  sample1 = matrix( rnorm(350, 0, i), 350, 1) # simulation 2 : var
-  
+  # sample1 = matrix( rnorm(350,(i-1),1), 350, 1) # mean
+  sample1 = matrix( rnorm(350, 0, i), 350, 1) # var
   stat1[i] = tst(sample0,sample1)
   wilcox1[i] = wilcox.test(sample0,sample1)$statistic
   kruskal1[i] = kruskal.test(c(sample0,sample1),c(rep(1,300),rep(2,350)))$statistic
@@ -18,11 +20,16 @@ for(i in 1:10){
   wass1[i] = wass_stat(sample0,sample1)
   dts1[i] = dts_stat(sample0,sample1)
   ad1[i] = ad_stat(sample0,sample1)
-  #wilcox2[i] = wilcoxtest(sample0,sample1)$statistic # simulation 1
+  # wilcox2[i] = wilcoxtest(sample0,sample1)$statistic # simulation 1
   vartest1[i] = vartest(sample0,sample1)$statistic # simulation 2
+  lr1[i] = twosample_test(sample0,sample1,doMethod = "LR")$statistics
+  za1[i] = twosample_test(sample0,sample1,doMethod = "ZA")$statistics
+  zk1[i] = twosample_test(sample0,sample1,doMethod = "ZK")$statistics
+  zc1[i] = twosample_test(sample0,sample1,doMethod = "ZC")$statistics
+  tnl1[i] = tnl.test(sample0,sample1,2)$statistic
 }
-x11(width = 12, height = 8); 
-par(mfrow=c(3,4))
+x11(width = 12, height = 8);
+par(mfrow=c(3,5))
 # x1 = seq(0,9); xlab1 = TeX(r'($u_1$)') # simulation 1
 x1 = 1:10; xlab1 = TeX(r'($\delta_1$)') # simulation 2
 plot(x1,stat1, xlab = xlab1, ylab = "statistic", main = "CE"); lines(x1,stat1)
@@ -34,5 +41,10 @@ plot(x1,kuiper1, xlab = xlab1, ylab = "statistic", main = "Kuiper");lines(x1,kui
 plot(x1,wass1, xlab = xlab1, ylab = "statistic", main = "WASS");lines(x1,wass1)
 plot(x1,dts1, xlab = xlab1, ylab = "statistic", main = "DTS");lines(x1,dts1)
 plot(x1,ad1, xlab = xlab1, ylab = "statistic", main = "AD");lines(x1,ad1)
-#plot(x1,wilcox2, xlab = xlab1, ylab = "statistic", main = "Wilcoxon2"); lines(x1,wilcox2) # simulation 1
+# plot(x1,wilcox2, xlab = xlab1, ylab = "statistic", main = "Wilcoxon2"); lines(x1,wilcox2) # simulation 1
 plot(x1,vartest1, xlab = xlab1, ylab = "statistic", main = "Vartest"); lines(x1,vartest1) # simulation 2
+plot(x1,lr1, xlab = xlab1, ylab = "statistic", main = "LR");lines(x1,lr1)
+plot(x1,za1, xlab = xlab1, ylab = "statistic", main = "ZA");lines(x1,za1)
+plot(x1,zk1, xlab = xlab1, ylab = "statistic", main = "ZK");lines(x1,zk1)
+plot(x1,zc1, xlab = xlab1, ylab = "statistic", main = "ZC");lines(x1,zc1)
+plot(x1,tnl1, xlab = xlab1, ylab = "statistic", main = "TNL");lines(x1,tnl1)
--- tst.r
+++ tst.r
@@ -10,11 +10,15 @@ library(kernlab)
 library(TwoSampleTest.HD)
 library(fasano.franceschini.test)
 library(Peacock.test)
-library(diproperm)
+#library(diproperm)
 library(RandomProjectionTest)
 library(DepthProc)
+library(depthTools)
 library(kSamples)
 library(lawstat)
+library(T4transport)
+library(rgTest)
+library(KMD)
 library(latex2exp)
 
 # mutual information based test
@@ -31,18 +35,18 @@ tst.mi<-function(s0,s1,n = 8){
 }
 
 m0 = c(0,0)
-rho = 0.5 # simulation 1
-# rho = 0.0 # simulation 2,3
+# rho = 0.5 # simulation 1
+rho = 0.0 # simulation 2,3
 v0 = matrix(c(1,rho,rho,1), nrow = 2)
 n0 = 400 # size of sample0
 n1 = 450 # size of sample1
 sample0 = rmnorm(n0,m0,v0)
 stat1 = kstat1 = stat2 = estat1 = ball1 = rf1 = hhg1 = hhg2 = hhg3 = hhg4 = cramer1 = tsthd1 = ff1 = peacock1 = dpp1 = rpt1 = rep(0,10)
-depth1 = ad1 = qn1 = bm1 = rep(0,10)
+depth1 = ad1 = qn1 = bm1 = wass1 = swdist1 = graph1 = kmd1 = rep(0,10)
 for(i in 1:10){
   # simulation 1
-  m1 = m0 + i - 1
-  sample1 = rmnorm(n1,m1,v0)
+  # m1 = m0 + i - 1
+  # sample1 = rmnorm(n1,m1,v0)
   
   # simulation 2
   # rho1 = (i-1) * 0.1
@@ -50,8 +54,8 @@ for(i in 1:10){
   # sample1 = rmnorm(n1,m0,v1)
   
   # simulation 3
-  # mv.cop <- mvdc(normalCopula(0.8), c("norm", "exp"), list(list(mean = 0, sd = 2), list(rate = i)))
-  # sample1 <- rMvdc(n1, mv.cop)
+  mv.cop <- mvdc(normalCopula(0.8), c("norm", "exp"), list(list(mean = 0, sd = 2), list(rate = i)))
+  sample1 <- rMvdc(n1, mv.cop)
 
   stat1[i] = tst(sample0,sample1)
   stat2[i] = tst.mi(sample0,sample1)
@@ -67,19 +71,23 @@ for(i in 1:10){
   tsthd1[i] = TwoSampleTest.HD(sample0,sample1)$statistic
   ff1[i] = fasano.franceschini.test(sample0,sample1)$statistic
   peacock1[i] = peacock2(sample0,sample1)
-  dpp1[i] = DiProPerm(rbind(sample0,sample1),c(rep(-1,n0),rep(1,n1)))$obs_teststat
+  #dpp1[i] = DiProPerm(rbind(sample0,sample1),c(rep(-1,n0),rep(1,n1)))$obs_teststat
   rpt1[i] = random_projection_test(sample0,sample1)$statistic
   depth1[i] = mWilcoxonTest(sample0,sample1)$statistic
   ad1[i] = ad.test(sample0,sample1)$ad[1,1]
   qn1[i] = qn.test(sample0,sample1)$qn[1]
   bm1[i] = brunner.munzel.test(sample0,sample1)$statistic
+  wass1[i] = wasserstein(sample0,sample1)$distance
+  swdist1[i] = swdist(sample0,sample1)$distance
+  graph1[i] = rg.test(sample0,sample1,n1 = n0, n2 = n1, weigh.fun = weiMax)$asy.wei.statistic
+  kmd1[i] = KMD_test(rbind(sample0,sample1),c(rep(1,n0),rep(2,n1)), Permutation = F)[1]
 }#i
 
 x11(width = 8, height = 12)
-par(mfrow = c(5,4))
-x1 = seq(0,9); xlab1 = TeX(r'($u_1$)') # simulation 1
+par(mfrow = c(6,4))
+# x1 = seq(0,9); xlab1 = TeX(r'($u_1$)') # simulation 1
 # x1 = seq(0,0.9,0.1); xlab1 = TeX(r'($\rho_1$)') # simulation 2
-# x1 = 1:10; xlab1 = "rate" # simulation 3
+x1 = 1:10; xlab1 = "rate" # simulation 3
 plot(x1,stat1, xlab = xlab1, ylab = "statistic", main = "CE");lines(x1,stat1)
 plot(x1,stat2, xlab = xlab1, ylab = "statistic", main = "MI");lines(x1,stat2)
 plot(x1,kstat1, xlab = xlab1, ylab = "statistic", main = "Kernel");lines(x1,kstat1)
@@ -94,9 +102,13 @@ plot(x1,cramer1, xlab = xlab1, ylab = "statistic", main = "Cramer");lines(x1,cra
 plot(x1,tsthd1, xlab = xlab1, ylab = "statistic", main = "TST.HD");lines(x1,tsthd1)
 plot(x1,ff1, xlab = xlab1, ylab = "statistic", main = "F-F");lines(x1,ff1)
 plot(x1,peacock1, xlab = xlab1, ylab = "statistic", main = "Peacock");lines(x1,peacock1)
-plot(x1,dpp1, xlab = xlab1, ylab = "statistic", main = "DiProPerm");lines(x1,dpp1)
+#plot(x1,dpp1, xlab = xlab1, ylab = "statistic", main = "DiProPerm");lines(x1,dpp1)
 plot(x1,rpt1, xlab = xlab1, ylab = "statistic", main = "RPT");lines(x1,rpt1)
 plot(x1,depth1, xlab = xlab1, ylab = "statistic", main = "Depth");lines(x1,depth1)
 plot(x1,ad1, xlab = xlab1, ylab = "statistic", main = "AD");lines(x1,ad1)
 plot(x1,qn1, xlab = xlab1, ylab = "statistic", main = "QN");lines(x1,qn1)
 plot(x1,bm1, xlab = xlab1, ylab = "statistic", main = "BM");lines(x1,bm1)
+plot(x1,wass1, xlab = xlab1, ylab = "statistic", main = "Wass");lines(x1,wass1)
+plot(x1,swdist1, xlab = xlab1, ylab = "statistic", main = "SWD");lines(x1,swdist1)
+plot(x1,graph1, xlab = xlab1, ylab = "statistic", main = "Graph");lines(x1,graph1)
+plot(x1,kmd1, xlab = xlab1, ylab = "statistic", main = "KMD");lines(x1,kmd1)
